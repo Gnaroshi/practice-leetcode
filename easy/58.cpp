60b73e1503df00232b3a6f179b3a88a3a9596e70
gnaroshi <gnaroshi@gmail.com>
2026-04-26 20:39:55 +0900

    58. Length of Last Word

---
+++ easy/58.cpp
@@ -0,0 +1,51 @@
+// problem: 58. Length of Last Word
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+class Solution {
+public:
+  int lengthOfLastWord(string s) {
+    stringstream st;
+    string t, l;
+    st.str(s);
+    while (st >> t) {
+    }
+    return t.size();
+  }
+};
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
