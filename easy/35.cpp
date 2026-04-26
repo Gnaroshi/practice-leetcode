b4f09c237a3859d94da0d7250cf31d9bc3b64c75
gnaroshi <gnaroshi@gmail.com>
2026-04-26 14:13:15 +0900

    35. Search Insert Position

---
+++ easy/35.cpp
@@ -0,0 +1,47 @@
+// problem: 35. Search Insert Position
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+class Solution {
+public:
+  int searchInsert(vector<int> &nums, int target) {
+    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+  }
+};
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
