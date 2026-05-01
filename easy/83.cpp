55e4daf09f8d9c09901a6d0bb6ef934fa6dbb0f1
gnaroshi <gnaroshi@gmail.com>
2026-05-01 12:01:13 +0900

    83. Remove Duplicates from Sorted List

---
+++ easy/83.cpp
@@ -0,0 +1,63 @@
+// problem: 83. Remove Duplicates from Sorted List
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpi = vector<pii>;
+using vvpi = vector<vpi>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using qi = queue<int>;
+using si = stack<int>;
+
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    auto current = head;
+    while (current != nullptr && current->next != nullptr) {
+      if (current->val == current->next->val) {
+        current->next = current->next->next;
+      } else {
+        current = current->next;
+      }
+    }
+    return head;
+  }
+};
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
