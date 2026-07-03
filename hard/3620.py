197497566e0a4aec26eff6347abd307235fca900
gnaroshi <gnaroshi@gmail.com>
2026-07-04 02:33:52 +0900

    3620. Network Recovery Pathways

---
+++ hard/3620.py
@@ -0,0 +1,66 @@
+"""
+Problem: 3620. Network Recovery Pathways
+"""
+
+import bisect
+import heapq
+import math
+from collections import Counter, defaultdict, deque
+from typing import Any, Dict, List, Optional, Set, Tuple
+
+
+class Solution:
+    def findMaxPathScore(
+        self, edges: List[List[int]], online: List[bool], k: int
+    ) -> int:
+        n = len(online)
+        g = [[] for _ in range(n)]
+        l, r = float("inf"), 0
+
+        for u, v, w in edges:
+            if not online[u] or not online[v]:
+                continue
+            g[u].append((v, w))
+            l = min(l, w)
+            r = max(r, w)
+
+        def check(mid: int) -> bool:
+            dis = [float("inf")] * n
+            pq = [(0, 0)]
+            dis[0] = 0
+
+            while pq:
+                d, u = heapq.heappop(pq)
+
+                if d > k:
+                    return False
+                if u == n - 1:
+                    return True
+                if d > dis[u]:
+                    continue
+
+                for v, w in g[u]:
+                    if w < mid:
+                        continue
+                    if dis[v] > dis[u] + w:
+                        dis[v] = dis[u] + w
+                        heapq.heappush(pq, (dis[v], v))
+
+            return False
+
+        if not check(l):
+            return -1
+
+        while l <= r:
+            mid = l + ((r - l) >> 2)
+            if check(mid):
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return r
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print()
