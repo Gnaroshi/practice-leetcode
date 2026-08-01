6e21ce10482e684e6d249d7de55c52fd14a725c3
gnaroshi <gnaroshi@gmail.com>
2026-08-01 19:26:44 +0900

    3016. Minimum Number of Pushes to Type Word II

---
+++ medium/3016.py
@@ -0,0 +1,26 @@
+"""
+Problem: 3016. Minimum Number of Pushes to Type Word II
+"""
+
+import bisect
+import heapq
+import math
+from collections import Counter, defaultdict, deque
+from typing import Any, Dict, List, Optional, Set, Tuple
+
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        cnt = sorted(Counter(word).items(), key=lambda x: (-x[1], x[0]))
+        idx = 0
+        ret = 0
+        for ch, cn in cnt:
+            ret += cn * (idx // 8 + 1)
+            idx += 1
+
+        return ret
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print()
