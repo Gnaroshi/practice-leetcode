c4984c58e051e165c22a991b6a2540943b18b234
gnaroshi <gnaroshi@gmail.com>
2026-08-03 13:39:22 +0900

    1406. Stone Game III

---
+++ hard/1406.py
@@ -0,0 +1,35 @@
+"""
+Problem: 1406. Stone Game III
+"""
+
+import bisect
+import heapq
+import math
+from collections import Counter, defaultdict, deque
+from typing import Any, Dict, List, Optional, Set, Tuple
+
+
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        dp = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            dp[i] = stoneValue[i] - dp[i + 1]
+            if i + 2 <= n:
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2])
+            if i + 3 <= n:
+                dp[i] = max(
+                    dp[i],
+                    stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3],
+                )
+
+        if dp[0] > 0:
+            return "Alice"
+        if dp[0] < 0:
+            return "Bob"
+        return "Tie"
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print()
