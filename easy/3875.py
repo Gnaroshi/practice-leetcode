dbac5df7879289078c82b2ba2a02d1d1391787ef
gnaroshi <gnaroshi@gmail.com>
2026-09-02 13:36:56 +0900

    3875. Construct Uniform Parity Array I

---
+++ easy/3875.py
@@ -0,0 +1,19 @@
+"""
+Problem: 3875. Construct Uniform Parity Array I
+"""
+
+import bisect
+import heapq
+import math
+from collections import Counter, defaultdict, deque
+from typing import Any, Dict, List, Optional, Set, Tuple
+
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print()
